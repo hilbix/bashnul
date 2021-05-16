02f8315e418316015311fba6de8ebc5bbffdb05e
Valentin Hilbig <webmaster@scylla-charybdis.com>
2021-05-17 00:39:51 +0200

    Test.sh for CirrusCI
    Fixes:
    
    make test
    ./Test.sh
    ./Test.sh: line 3: /usr/bin/sudo: Permission denied
    cmp: /usr/bin/sudo: Permission denied
    real	0m4.864s
    user	0m2.641s
    sys	0m4.821s

--- Test.sh
+++ Test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-time for a in /usr/bin/*; do [ -f "$a" ] || continue; LC_ALL=C ./bashnul -e <"$a" | LC_ALL=C ./bashnul -d | cmp - "$a"; done
+time for a in /usr/bin/*; do [ -f "$a" ] && [ -r "$a" ] || continue; LC_ALL=C ./bashnul -e <"$a" | LC_ALL=C ./bashnul -d | cmp - "$a" || break; done
 
